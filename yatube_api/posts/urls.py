d73ce12c361fe23c99a002b1c031a11c1f2ce641
“Елена <borovina@bk.ru>
2022-03-06 11:01:31 +0300

    «8»

--- yatube_api/posts/urls.py
+++ yatube_api/posts/urls.py
@@ -15,5 +15,5 @@ router.register(
 
 urlpatterns = [
     path('v1/', include(router.urls)),
-    path('v1/api-token-auth/', views.obtain_auth_token),
+    path('/api/v1/api-token-auth/', views.obtain_auth_token),
 ]
